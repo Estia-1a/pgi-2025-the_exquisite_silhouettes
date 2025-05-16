21fbae52461f6a2586068153791fed21a8e87b8e
EnzoMaluvin <190848795+EnzoMaluvin@users.noreply.github.com>
2025-05-16 15:31:11 +0200

    modi ticket 1

--- src/features.c
+++ src/features.c
@@ -12,6 +12,17 @@
  */
 
 
-void dimension(char *adresse) {
-    
-}
+ void dimension(char *adresse);
+ unsigned char *read_image_data=NULL;
+ int largeur=0,hauteur=0,c=0;
+ 
+ if (read_image_data(adresse,&w,&h,&c)){
+     printf("dimension: %d, %d",w,h);
+     else{
+         fprintf(stderr,"nique ta photo ya une erreur sorry brother",adresse);
+     }
+ }
+ if (data!=NULL){
+     free(data);
+ }
+ 
--- src/features.h
+++ src/features.h
@@ -2,19 +2,10 @@
 #define FEATURES_H
 
 
-void dimension(char *adresse);
-unsigned char *read_image_data=NULL;
-int largeur=0,hauteur=0,c=0;
+void dimension(char *adresse) {
+    
+}
 
-if (read_image_data(adresse,&w,&h,&c)){
-    printf("dimension: %d, %d",w,h);
-    else{
-        fprintf(stderr,"nique ta photo ya une erreur sorry brother",adresse);
-    }
-}
-if (data!=NULL){
-    free(data);
-}
 
 
 #endif
